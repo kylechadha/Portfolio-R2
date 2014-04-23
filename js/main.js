7fae4128c92a30f5c3df53a0d8acb77a41fa366f
Kyle Chadha <kyle.chadha@gmail.com>
2014-04-22 21:49:53 -0400

    Reorganize main.js file

--- js/main.js
+++ js/main.js
@@ -1,84 +1,31 @@
 $(document).ready(function() {
     "use strict";
-  $('#contact-form').submit(function(){
-    var form = $(this);
-    $.ajax({
-      type: 'POST',
-      url: base_url+'/ajax.php',
-      dataType: 'json',
-      data: {
-        'user_name':$('#user_name').val(),
-        'user_email':$('#user_email').val(),
-        'user_message':$('#user_message').val()
-      },
-      success: function(data){
-        form.find(".error").removeClass('error');
-        if(data.error){
-          for(var i in data.error){
-            $("#"+data.error[i]).prev().addClass('error')
-          }
-        } else {
-          for(var i in data.succ){
-            $("#"+i).val(data.succ[i].def)
-          }
-          $("#contact-form label").removeClass('error');
-          $("#contact-form").hide(0);
-          $("#contact .msg").show(0);
+
+  // Intro Cover: Parallax 
+  if (jQuery(window).width() > 800) {
+    
+    var counter = 0;
+    var startPosition;
+    var xPosition;
+
+    $('#parallax').mousemove(function(e){
+
+        if (counter == 0) { startPosition = e.pageX };
+
+        xPosition = (e.pageX - startPosition)/10 + 30;
+        if (xPosition < 0) {
+          xPosition = 0;
+        } else if (xPosition > 100) {
+          xPosition = 100;
         }
-      }
-    })
-    return false;
-  });
-  
-  function responsive() {
-    $(".win-height").height($(window).height())
-    if ($(window).width()<600) {
-      $('.skill .progress-line').removeClass("disa");
-    }
-  }
-  
-  responsive()
-  $(window).resize(function() {
-    responsive()
-  });
 
-  $('.bxslider').bxSlider({
-    auto: true,
-    pause: 5000
-  });
-  
-  var offset = $(".holder-head").offset();
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > offset.top ) {
-      $(".holder-head").addClass("moved")
-    } else {
-      $(".holder-head").removeClass("moved")
-    };
-    if ($(window).scrollTop() > $(".skill .right").offset().top-$(window).height()/1.4 ) {
-      $('.skill .progress-line').removeClass("disa");
-    };
-  }); 
+        $(this).css('background-position', xPosition + "%" + " 50%");
+        console.log(startPosition);
+        counter += 1;
+    });
+  };
 
-  $('#menu').onePageNav();
-  
-  $(".scrollto").click(function() {
-      $('html, body').animate({
-          scrollTop: $($(this).attr('href')).offset().top-70
-      }, 600);
-      return false;
-  });
-
-  // Skills Section: Superscrollorama Plugin
-  var controller = $.superscrollorama({
-    reverse: false
-  });
-  controller.addTween('.about .info section article.web', TweenMax.from( $('.about .info section article.web'), .5, {css:{opacity: 0}}));
-  controller.addTween('.about .info section article.graphic', TweenMax.from( $('.about .info section article.graphic'), .5, {css:{opacity: 0}}));
-  controller.addTween('.about .info section article.marketing', TweenMax.from( $('.about .info section article.marketing'), .5, {css:{opacity: 0}}));
-  controller.addTween('.about .info section article.seo', TweenMax.from( $('.about .info section article.seo'), .5, {css:{opacity: 0}}));
-
-
-  // Portfolio Section: General Setup
+  // Portfolio Section: General Configuration
   function equalHeight(group) {
     var tallest = 0;
     group.each(function() {
@@ -121,17 +68,11 @@ $(document).ready(function() {
             type: 'inside'
         }
     }
-    // 'titleShow'   : true,
-    // 'titlePosition' : "over"
   });
-
 
   // Portfolio Section: Isotope Plugin
   var $container = $('#projects');
   $container.isotope({
-    // options
-    // itemSelector: '.item',
-    // layoutMode: 'fitRows'
   });
 
   $('#filters li a').click(function(){
@@ -139,27 +80,79 @@ $(document).ready(function() {
     $container.isotope({ filter: selector });
     return false;
   });
+  
+  // Skills Section: Superscrollorama Plugin
+  var controller = $.superscrollorama({
+    reverse: false
+  });
+  controller.addTween('.about .info section article.web', TweenMax.from( $('.about .info section article.web'), .5, {css:{opacity: 0}}));
+  controller.addTween('.about .info section article.graphic', TweenMax.from( $('.about .info section article.graphic'), .5, {css:{opacity: 0}}));
+  controller.addTween('.about .info section article.marketing', TweenMax.from( $('.about .info section article.marketing'), .5, {css:{opacity: 0}}));
+  controller.addTween('.about .info section article.seo', TweenMax.from( $('.about .info section article.seo'), .5, {css:{opacity: 0}}));
 
-
-  // Banner: Parallax 
-  if (jQuery(window).width() > 800) {
-    var counter = 0;
-    var startPosition;
-    var xPosition;
-    $('#parallax').mousemove(function(e){
-
-        if (counter == 0) { startPosition = e.pageX };
-
-        xPosition = (e.pageX - startPosition)/10 + 30;
-        if (xPosition < 0) {
-          xPosition = 0;
-        } else if (xPosition > 100) {
-          xPosition = 100;
+  // Contact: Form Mailer
+  $('#contact-form').submit(function(){
+    var form = $(this);
+    $.ajax({
+      type: 'POST',
+      url: base_url+'/ajax.php',
+      dataType: 'json',
+      data: {
+        'user_name':$('#user_name').val(),
+        'user_email':$('#user_email').val(),
+        'user_message':$('#user_message').val()
+      },
+      success: function(data){
+        form.find(".error").removeClass('error');
+        if(data.error){
+          for(var i in data.error){
+            $("#"+data.error[i]).prev().addClass('error')
+          }
+        } else {
+          for(var i in data.succ){
+            $("#"+i).val(data.succ[i].def)
+          }
+          $("#contact-form label").removeClass('error');
+          $("#contact-form").hide(0);
+          $("#contact .msg").show(0);
         }
+      }
+    })
+    return false;
+  });
+  
+  // Sitewide Configurations
+  function responsive() {
+    $(".win-height").height($(window).height())
+    if ($(window).width()<600) {
+      $('.skill .progress-line').removeClass("disa");
+    }
+  }
+  
+  responsive()
+  $(window).resize(function() {
+    responsive()
+  });
+  
+  var offset = $(".holder-head").offset();
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > offset.top ) {
+      $(".holder-head").addClass("moved")
+    } else {
+      $(".holder-head").removeClass("moved")
+    };
+    if ($(window).scrollTop() > $(".skill .right").offset().top-$(window).height()/1.4 ) {
+      $('.skill .progress-line').removeClass("disa");
+    };
+  }); 
 
-        $(this).css('background-position', xPosition + "%" + " 50%");
-        console.log(startPosition);
-        counter += 1;
-    });
-  };
+  $('#menu').onePageNav();
+  
+  $(".scrollto").click(function() {
+      $('html, body').animate({
+          scrollTop: $($(this).attr('href')).offset().top-70
+      }, 600);
+      return false;
+  });
+
 });
